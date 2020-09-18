d6785e828492e02e317fcb52c25ae5d711c7b963
il12 <isl5498dze>
2020-09-18 20:33:54 +0300

    Posts on main page redesigned

--- page-templates/tpl_index.php
+++ page-templates/tpl_index.php
@@ -7,13 +7,16 @@
 <section class="content">
     <div id="home" class="grid post">
         <div class="unit whole">
-        <ul>
+        <ul class='grid'>
         <?php
             $the_query = new WP_Query( 'posts_per_page=5' );
             while ($the_query -> have_posts()) : $the_query -> the_post();
         ?>
-            <li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
-            <li><?php the_excerpt(__('(more…)')); ?></li>
+            <li class='unit'>
+                <div><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></div>
+                <hr>
+                <div><?php the_excerpt(__('(more…)')); ?></div>
+            </li>
         <?php
             endwhile;
             wp_reset_postdata();
