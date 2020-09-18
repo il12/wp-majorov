617bbd5b2579a5da0a132e11442c2ea08c821829
il12 <isl5498dze>
2020-09-18 19:51:04 +0300

    Redesigned previous conferences; First attempt to add latest posts on home page

--- page-templates/tpl_conf2019.php
+++ page-templates/tpl_conf2019.php
@@ -1,5 +1,5 @@
 <?php
-    /* Template Name: Previous Conference Page */
+    /* Template Name: 2019 Conference Page */
     /* Template Post Type: post, page, event */
 ?>
 <?php get_header()?>
@@ -65,49 +65,6 @@
                 <img src="<?php echo get_template_directory_uri();?>/img/IMG_7508.jpg">
                 <img src="<?php echo get_template_directory_uri();?>/img/IMG_7521.jpg">
                 <img src="<?php echo get_template_directory_uri();?>/img/IMG_7541.jpg"></p>
-
-            <h2 id="micsecs-2018">MICSECS-2018</h2>
-
-            <p>Proceedings in CEUR (indexed by Scopus): <a href="http://ceur-ws.org/Vol-2344/" target="_blank"><strong>jump
-                to proceedings</strong></a></p>
-
-            <p>Printed Russian version: <a href="<?php echo get_template_directory_uri();?>/media/MICSECS_RUS_2018.pdf" target="_blank"><strong>download</strong></a>
-            </p>
-
-            <p><strong>Conference statistics</strong></p>
-
-            <ul>
-                <li>Submissions: 112</li>
-                <li>Accepted submissions: 82</li>
-                <li>Acceptance rate: 0.73</li>
-                <li>Participants: 273
-                    <!-- * Participants (under 39): 172 --></li>
-            </ul>
-
-            <p><strong>Keynote talk</strong>: «Self-Driving Cars: Difficulties vs Opportunities? » by <strong>Boris
-                Ivanov</strong>, <a href="https://www.starline.ru/" target="_blank"><strong>StarLine</strong></a></p>
-
-            <p><img src="<?php echo get_template_directory_uri();?>/img/IMG_8931.jpg"></p>
-
-            <p>Poster &amp; Demo Session</p>
-
-            <p><img src="<?php echo get_template_directory_uri();?>/img/IMG_8945.jpg"></p>
-
-            <p><img src="<?php echo get_template_directory_uri();?>/img/IMG_8968.jpg"></p>
-
-            <p><img src="<?php echo get_template_directory_uri();?>/img/IMG_8993.jpg"></p>
-
-            <p><img src="<?php echo get_template_directory_uri();?>/img/IMG_8998.jpg"></p>
-
-            <p><img src="<?php echo get_template_directory_uri();?>/img/IMG_9072.jpg"></p>
-
-            <p><img src="<?php echo get_template_directory_uri();?>/img/IMG_9076.jpg"></p>
-
-            <p><strong>Keynote talk</strong>: «Self Sovereign Identity: on DIDs and more», by <strong>Dirk
-                Thatmann</strong>, <a href="https://laboratories.telekom.com/"
-                                      target="_blank"><strong>T-Labs</strong></a></p>
-
-            <p><img src="<?php echo get_template_directory_uri();?>/img/IMG_9150.jpg"></p>
         </div>
     </div>
 </section>
