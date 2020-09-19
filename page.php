eb2d46b1604526434aecce673fc10d521574f3a9
il12 <il5498@yandex.ru>
2020-09-19 14:33:46 +0300

    Page title fixed

--- page.php
+++ page.php
@@ -7,7 +7,7 @@
 <section class="content">
     <div class="grid post">
         <div class="unit whole">
-                <h2><?php wp_title(); ?></h2>
+                <h2><?php wp_title("", true); ?></h2>
                 <?php the_content(); ?>
         </div>
     </div>
